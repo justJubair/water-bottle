9f28fd8e93b75b9e1e40b799d3674c0410c059ea
Jubair <jubair.ahmed2838@gmail.com>
2023-09-10 23:56:37 +0600

    shopping card added

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handlePurchase}) => {
     const {name, price, img, seller}=bottle
-    console.log(bottle)
+    
   return (
     <div>
         <p className='font-semibold text-xl mb-4'>{name}</p>
         <img className='w-64' src={img} alt="" />
         <p className='font-semibold'>Price: ${price}</p>
-        <p>Seller: {seller}</p>
+        <p className='my-2'>Seller: {seller}</p>
+        <button onClick={()=>{handlePurchase(bottle)}} className='bg-pink-200'>Purchase</button>
     </div>
   )
 }
